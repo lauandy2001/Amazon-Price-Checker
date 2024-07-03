e6a334f64b9c60079e19f97757067d8a3f490a13
lauandy2001 <lauandy2001@gmail.com>
2024-07-02 21:38:52 -0400

    Complete scrapper

--- core.py
+++ core.py
@@ -1,26 +1,56 @@
 import requests
 import smtplib
+import time
 from bs4 import BeautifulSoup
 
-def check_price:
 
-    URL = "https://www.amazon.com/VIVAGLORY-Flotation-Durable-Adjustable-Fastening/dp/B07QTPB1F1/?_encoding=UTF8&pd_rd_w=4fqxG&content-id=amzn1.sym.e92d427b-6681-44e4-9201-8463289b9c75%3Aamzn1.symc.f3a6ae52-fb92-4bbd-ba18-628777ebc1c0&pf_rd_p=e92d427b-6681-44e4-9201-8463289b9c75&pf_rd_r=QJEB7PZ8S33J0JP1B7HX&pd_rd_wg=85Qre&pd_rd_r=1ac3bcc0-b80d-4d94-97bb-9dc1d461b6a7&ref_=pd_hp_d_atf_ci_mcx_mr_ca_hp_atf_d'}
-    headers = {"User-Agent" : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"} 
 
-    AmazonPage = requests.get(URL,headers = headers)
+URL = 'https://www.amazon.com/Kuoser-Reflective-Adjustable-Visibility-Lifesaver/dp/B091TN8WWK/ref=sr_1_2?crid=2C2TXYBIR0Z3B&dib=eyJ2IjoiMSJ9.zGd3lLBP-XL6Od1rvDaUMRzKXqWruLeePk7J-oRVED9W9QWqM1wKAW_9iotCvY_FrXN5ucbkxr-bPTmLF4Z0nV8l1lJn0LIYH8nN9sU22po2Tq-Xsc7Ly27POqr88zcLAtsK8HQOyq8KXIrmrjoiGJXtA0UuwMIta_wePqhjrNFvt90jC_bf3nwh0_3ySgYmVxDWHGiXefx8RtjY7dDV-tatmc1VvF6WrIayvtT3yHUWjt10MtX2EfUNutdlpvlhycnr_SUJicXqE-R1gHH299AZmhSpGhwaTNLK5lxoHGU.Hyd2FCAsC9kWdBXVt-1j6FWYEDqbU49zMWNMdX5tAWo&dib_tag=se&keywords=dog+life+vest+xl&qid=1719964677&sr=8-2'
+headers = {"User-Agent" : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36'} 
+
+page = requests.get(URL,headers = headers)
+
+
+deal_price = None
+def get_price():
+    global deal_price
+    soup = BeautifulSoup(page.content,'html.parser')
+    deal_price = soup.select_one(".a-offscreen").getText()
+    print(deal_price)
+
+while deal_price is None:
+    try:
+        get_price()
+    except AttributeError:
+        print("Error fetching price. Retrying...")
+        time.sleep(5)  # Adding a delay of 5 seconds before retrying
+'''
+    AmazonPage.raise_for_status()
+    data = AmazonPage.text
+    print(data)
 
     soupInfo = BeautifulSoup(AmazonPage.content, 'html.parser')
+    title = soupInfo.find(id='productTitle').get_text()
+    #price1 = soupInfo.find(class_= "a-price-whole").get_text()
+    #price2 = soupInfo.find(class_= "a-price-fraction").get_text()
 
-    title = soup.find(id='productTitle').get_text()
-    price1 = soup.find(class = 'a-price-whole').get_text()
-    price2 = soup.find(class = 'a-price-fraction').get_text()
+    print(title)
+
+while title is None:
+    try:
+        get_price()
+    except AttributeError:
+        print("Error fetching price. Retrying...")
+        time.sleep(5)  # Adding a delay of 5 seconds before retrying
+
     
+   
     if(converted_price < 1.700):
         send_mail()
 
     print(converted_price)
     print(title.strip())
-
+'''
 
 def send_mail()
     server = smtplib.SMTP('smtp.gmail.com',587)
@@ -42,3 +72,5 @@ def send_mail()
     print("Mail sent")
 
     server.quit()
+
+
